91232550e9e83022c4221d845f5977bc4ba62021
Kent Collins <kent.r.collins@gmail.com>
2016-12-20 14:33:22 +0000

    renamed ssr pins

--- src/weaveit2me/raspberry/RPiLoomController.java
+++ src/weaveit2me/raspberry/RPiLoomController.java
@@ -7,9 +7,9 @@ import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.GpioPinPwmOutput;
-import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.wiringpi.Gpio;
 
 import weaveit2me.core.LoomController;
 
@@ -22,32 +22,31 @@ import weaveit2me.core.LoomController;
 public class RPiLoomController implements LoomController {
 
 	private static RPiLoomController currentInstance = null;
-	private static int MAX_SHAFTS; // affects shift register ops
+	private static int MAX_SHAFTS; // informs shift register ops
 	private static Integer[] selected; // current selection
 
 	private static final GpioController gpio = GpioFactory.getInstance();
 
-	private GpioPinDigitalOutput dataPin, shiftPin, strobePin, oenablePin;
-	private GpioPinDigitalOutput servoPin;
+	private GpioPinDigitalOutput ssrData; // serial data 
+	private GpioPinDigitalOutput ssrClock; // clock performs shift
+	private GpioPinDigitalOutput ssrStrobe; // latches (stores) data
+	private GpioPinDigitalOutput ssrOEnable; // makes data available
+	private GpioPinDigitalOutput servoEnable; // enables HC153s
 
 	/**
 	 * Singleton controller using default pin assignments.
 	 */
 	private RPiLoomController() {
-		// data pin provides serial input signals
-		dataPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, PinState.LOW);
-		dataPin.setShutdownOptions(true, PinState.LOW);
-		// shift pin (clock) advances signals internally
-		shiftPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, PinState.LOW);
-		shiftPin.setShutdownOptions(true, PinState.LOW);
-		// strobe pin latches current values to the register
-		strobePin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, PinState.LOW);
-		strobePin.setShutdownOptions(true, PinState.LOW);
-		// oenable pin makes values available
-		oenablePin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, PinState.LOW);
-		oenablePin.setShutdownOptions(true, PinState.LOW);
-		servoPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, PinState.LOW);
-		servoPin.setShutdownOptions(true, PinState.LOW);
+		ssrData = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, PinState.LOW);
+		ssrData.setShutdownOptions(true, PinState.LOW);
+		ssrClock = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, PinState.LOW);
+		ssrClock.setShutdownOptions(true, PinState.LOW);
+		ssrStrobe = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, PinState.LOW);
+		ssrStrobe.setShutdownOptions(true, PinState.LOW);
+		ssrOEnable = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, PinState.LOW);
+		ssrOEnable.setShutdownOptions(true, PinState.LOW);
+		servoEnable = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, PinState.LOW);
+		servoEnable.setShutdownOptions(true, PinState.LOW);
 	}
 
 	/**
@@ -86,11 +85,9 @@ public class RPiLoomController implements LoomController {
 	@Override
 	public void openShed() {
 		SortedSet<Integer> orderedPicks = sanitizeSelection();
-		loadDataIntoShiftRegister(orderedPicks);
-		latchShaftSelections();
-		enableShaftPositioners();
+		loadShaftDataRegister(orderedPicks);
+		latchSelectedShafts();
 		lift();
-		disableShaftPositioners();
 	}
 
 	private SortedSet<Integer> sanitizeSelection() {
@@ -106,17 +103,17 @@ public class RPiLoomController implements LoomController {
 		return picks;
 	}
 
-	private void loadDataIntoShiftRegister(SortedSet<Integer> orderedPicks) {
-		dataPin.setState(PinState.LOW);
-		shiftPin.setState(PinState.LOW);
-		strobePin.setState(PinState.LOW);
-		oenablePin.setState(PinState.LOW);
+	private void loadShaftDataRegister(SortedSet<Integer> orderedPicks) {
+		ssrData.setState(PinState.LOW);
+		ssrClock.setState(PinState.LOW);
+		ssrStrobe.setState(PinState.LOW);
+		ssrOEnable.setState(PinState.LOW);
 		for (Integer i = 1; i <= MAX_SHAFTS; i++) {
 			if (orderedPicks.contains(i)) {
-				dataPin.setState(PinState.HIGH);
+				ssrData.setState(PinState.HIGH);
 
 			} else {
-				dataPin.setState(PinState.LOW);
+				ssrData.setState(PinState.LOW);
 			}
 			try {
 				Thread.sleep(1);
@@ -124,41 +121,48 @@ public class RPiLoomController implements LoomController {
 				e1.printStackTrace();
 			}
 			// provide rising clock edge
-			shiftPin.setState(PinState.HIGH);
+			ssrClock.setState(PinState.HIGH);
 			try {
 				Thread.sleep(1);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			shiftPin.setState(PinState.LOW);
+			ssrClock.setState(PinState.LOW);
 		}
-		strobePin.setState(PinState.HIGH);
+		ssrStrobe.setState(PinState.HIGH);
 		try {
 			Thread.sleep(1);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		strobePin.setState(PinState.LOW);
-		oenablePin.setState(PinState.HIGH);
+		ssrStrobe.setState(PinState.LOW);
+		ssrOEnable.setState(PinState.HIGH);
 	}
 
-	private void latchShaftSelections() {
-		// Apply latching signal to the register
-		// enable output
+	private void latchSelectedShafts() {
+		enableServos();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		disableServos();
 
 	}
 
-	private void enableShaftPositioners() {
-		// TODO Auto-generated method stub
+	private void enableServos() {
+		// mux is low enable
+		servoEnable.setState(PinState.LOW);
+	}
+
+	private void disableServos() {
+		// no signal to servos
+		servoEnable.setState(PinState.HIGH);
 
 	}
 
 	private void lift() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void disableShaftPositioners() {
 		// TODO Auto-generated method stub
 
 	}
@@ -209,30 +213,22 @@ public class RPiLoomController implements LoomController {
 	public void custom(String s) {
 		System.out.println("Received custom command " + s);
 		String[] commands = s.split(" ");
-		String arg1 = commands[0].trim().toUpperCase();
-		String arg2 = commands[1].trim().toUpperCase();
+		String arg1 = commands.length > 0 ? commands[0].trim().toUpperCase() : "ARG1";
+		String arg2 = commands.length > 1 ? commands[1].trim().toUpperCase() : "ARG2";
 		GpioPinDigitalOutput chosen = null;
 		PinState state = null;
 		if (arg1.equals("STROBE")) {
-			chosen = shiftPin;
+			chosen = ssrClock;
 		} else if (arg1.equals("CLOCK")) {
-			chosen = strobePin;
+			chosen = ssrStrobe;
 		} else if (arg1.equals("DATA")) {
-			chosen = dataPin;
+			chosen = ssrData;
 		} else if (arg1.equals("OE")) {
-			chosen = oenablePin;
-		} else if (arg1.equals("SERVO")) {
-			if (commands.length<3) return;
-			String arg3 = commands[2].trim().toUpperCase();
-			System.out.println(arg1+":"+arg2+":"+arg3);
-			long timeInMillis = Long.parseLong(arg3);
-			if (arg2.equals("HIGH")){
-				System.out.println("Sending servo high");
-				pulseServoHigh(timeInMillis);
-			} else if (arg2.equals("LOW")){
-				System.out.println("Sending servo low");
-				pulseServoLow(timeInMillis);
-			}
+			chosen = ssrOEnable;
+		} else if (arg1.equals("LATCH")) {
+			enableServos();
+		} else if (arg1.equals("UNLATCH")) {
+			disableServos();
 		}
 		if (arg2.equals("HIGH")) {
 			state = PinState.HIGH;
@@ -243,44 +239,6 @@ public class RPiLoomController implements LoomController {
 			System.out.println("Setting " + arg1 + " to " + arg2);
 			chosen.setState(state);
 		}
-
-	}
-
-	private void pulseServoHigh(long millis) {
-		long secondsToSpin = 1_000_000 * millis;
-		long startTime = System.nanoTime();
-		while (System.nanoTime() < startTime + secondsToSpin) {
-			try {
-				servoPin.setState(PinState.HIGH);
-				// 2400 us = 2ms and 400_000 ns
-				Thread.sleep(2, 400_000);
-				servoPin.setState(PinState.LOW);
-				Thread.sleep(17, 600_000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-	}
-
-	private void pulseServoLow(long millis) {
-		long reqDuration = 1_000_000L * millis;
-		long startTime = System.nanoTime();
-		while (System.nanoTime() < startTime + reqDuration) {
-			try {
-				servoPin.setState(PinState.HIGH);
-				// 554 us = 554_000 ns
-				Thread.sleep(0,554_000);
-				servoPin.setState(PinState.LOW);
-				// 19,446 us = 19 ms and 446_000ns
-				Thread.sleep(19, 446_000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		System.out.println("Started servo high "+startTime+"\nconcluded at "+System.nanoTime());
 
 	}
 
