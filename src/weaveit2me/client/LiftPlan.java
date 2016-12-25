59c0cffcf3b402a37d2ef8e7c4403116877dc1fd
Kent Collins <kent.r.collins@gmail.com>
2016-12-25 23:27:45 +0000

    using file resources

--- src/weaveit2me/client/LiftPlan.java
+++ src/weaveit2me/client/LiftPlan.java
@@ -1,4 +1,4 @@
-package weaveit2me.core;
+package weaveit2me.client;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -6,21 +6,22 @@ import java.util.List;
 import java.util.Map;
 
 /***
- * Processes a WIF formatted file to generate a LiftPlan. The file 
+ * Processes a WIF formatted file to generate a LiftPlan. The file
  * provided must either specify a [LIFTPLAN] or provide both [TIEUP]
  * and [TREADLING] sections.
  *
  */
 
 public class LiftPlan {
-	
+
 	Map<Integer, List<Integer>> picks;
 
 	public LiftPlan() {
+		this("/Default.wif");
 	}
 
 	public LiftPlan(String fileName) {
-		Map<String, HashMap<String, String>> info = WIFReader.parseFile(fileName);
+		Map<String, HashMap<String, String>> info = WIFReader.parseResource(fileName);
 		if (info.containsKey("LIFTPLAN")) {
 			initializePicks(info.get("LIFTPLAN"));
 		} else if (info.containsKey("TIEUP") && info.containsKey("TREADLING")) {
@@ -43,26 +44,27 @@ public class LiftPlan {
 			if (!picks.containsKey(treadle)) {
 				picks.put(treadle, new ArrayList<Integer>());
 			}
-			for (Integer tieup: treadles.get(treadle)) {
+			for (Integer tieup : treadles.get(treadle)) {
 				picks.get(treadle).addAll(tieups.get(tieup));
 			}
 		}
 
 	}
-	
+
 	private static Map<Integer, List<Integer>> toIntegers(HashMap<String, String> stringMap) {
 		Map<Integer, List<Integer>> intMap = new HashMap<Integer, List<Integer>>();
-		for (String stringKey: stringMap.keySet()) {
+		for (String stringKey : stringMap.keySet()) {
 			Integer intKey = Integer.parseInt(stringKey.trim());
-			if (!intMap.containsKey(intKey)) intMap.put(intKey, new ArrayList<Integer>());
+			if (!intMap.containsKey(intKey))
+				intMap.put(intKey, new ArrayList<Integer>());
 			String[] stringVals = stringMap.get(stringKey).split(",");
-			for (String i: stringVals) {
+			for (String i : stringVals) {
 				intMap.get(intKey).add(Integer.parseInt(i.trim()));
 			}
 		}
 		return intMap;
 	}
-	
+
 	/**
 	 * @return the picks
 	 */
@@ -71,16 +73,16 @@ public class LiftPlan {
 	}
 
 	/**
-	 * @param picks the picks to set
+	 * @param picks
+	 *            the picks to set
 	 */
 	public void setPicks(Map<Integer, List<Integer>> picks) {
 		this.picks = picks;
 	}
 
 	public static void main(String[] args) {
-		//Map<String, HashMap<String, String>> info = WIFReader.parseFile("DDW-Multi with Dobby.wif");
-		LiftPlan p = new LiftPlan("Default.wif");
-		//LiftPlan p = new LiftPlan("DDW-Multi with Dobby.wif");
+		// loads the default wif file
+		LiftPlan p = new LiftPlan();
 		System.out.println(p.getPicks());
 	}
 
--- src/weaveit2me/client/WIFReader.java
+++ src/weaveit2me/client/WIFReader.java
@@ -1,7 +1,8 @@
-package weaveit2me.core;
+package weaveit2me.client;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -41,6 +42,39 @@ public class WIFReader {
 			e.printStackTrace();
 		}
 
+		return info;
+	}
+
+	public static Map<String, HashMap<String, String>> parseResource(String fileName) {
+		InputStream is = WIFReader.class.getResourceAsStream(fileName);
+		Map<String, HashMap<String, String>> info = new HashMap<String, HashMap<String, String>>();
+		String section = null;
+		try (Scanner scan = new Scanner(is)) {
+			while (scan.hasNextLine()) {
+				String s = scan.nextLine().trim();
+				if (s.indexOf(";") >= 0) {
+					s = s.substring(0, s.indexOf(";"));
+				}
+				if (s.equals("")) {
+					section = null;
+					continue;
+				} else if (isSection(s)) {
+					int leftBracket = s.indexOf("[");
+					int rightBracket = s.indexOf("]");
+					section = s.substring(leftBracket + 1, rightBracket);
+					section = section.toUpperCase();
+					info.put(section, new HashMap<String, String>());
+				} else { // within a section
+					int equals = s.indexOf("=");
+					if (equals > -0) { // found a new mapping
+						String subkey = s.substring(0, equals);
+						String subval = s.substring(equals + 1);
+						info.get(section).put(subkey, subval);
+					}
+				}
+
+			}
+		}
 		return info;
 	}
 
